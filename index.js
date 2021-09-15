ef87c06219d55d3d9974778ed2c7c2190b81a559
AnaNicoletti <>
2021-09-15 17:53:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const porta = 3000;
+
+app.use(express.static ('public')); 
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => {
+  res.render("index.ejs");
+})
+
+app.get('/lista', (req, res) => {
+  res.render("lista/notícias.ejs");
+})
+
+app.get('/noticia01', (req, res) => {
+  res.render("noticia/news01.ejs");
+})
+
+app.get('/noticia02', (req, res) => {
+  res.render("noticia/news02.ejs");
+})
+
+app.get('/noticia03', (req, res) => {
+  res.render("noticia/news03.ejs");
+})
+
+app.listen(porta, () => {
+  console.log("O servidor está funcionando")
+})
